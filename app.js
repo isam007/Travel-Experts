9d946801bd032b11ff72620a538cf94dd363113c
Karim Khan <kakhan87@gmail.com>
2020-03-18 14:57:45 -0600

    latest update following our progress meeting

--- app.js
+++ app.js
@@ -5,41 +5,51 @@ const bodyParser = require("body-parser");
 var data = [];
 const app = express();
 
+// Create Server
 app.listen(8000, err=>{
 	if (err) throw err;
-	console.log("server started");
+	console.log("Server started on port 8000");
 });
 
+// Express static pages - HTML/CSS/JS
 app.use(express.static("views"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Call different pages - Home/Packages/Register/Contact/404
+//  Home Page
 app.get("/", (req, res)=>{
 	res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/index", (req, res)=>{
-	res.sendFile(__dirname + "/index.html");
+// Packages Page
+app.get("/holiday-packages", (req, res)=>{
+	res.sendFile(__dirname + "/holiday_packages.html");
 });
 
+// Register Page
 app.get("/register", (req, res)=>{
 	res.sendFile(__dirname + "/register.html");
 });
 
+// Contact Page
 app.get("/contact-us", (req, res)=>{
 	res.sendFile(__dirname + "/contact.html");
 });
 
-app.get("/", (req, res)=>{
+// 404 Page
+app.get("*", (req, res)=>{
 	res.sendFile(__dirname + "/404.html");
 });
-    
+
+// Thank you Page
 app.get("/thanks", (req, res)=>{
 	res.sendFile(__dirname + "/thanks.html");
 });
 
+// Registration Form
 app.post("/post_form", (req, res)=>{
-	console.log(req.body);
+	console.log(req.body	);
 	data[0] = req.body.CustFirstName;
 	data[1] = req.body.CustLastName;
 	data[2] = req.body.CustAddress;
@@ -53,7 +63,7 @@ app.post("/post_form", (req, res)=>{
 	data[10] = req.body.AgentId;
 	var conn = mysql.createConnection({
 		host: "localhost",
-		user: "Anas",
+		user: "Wintech",
 		password: "password",
 		database: "travelexperts"
 	});
@@ -68,10 +78,57 @@ app.post("/post_form", (req, res)=>{
 		conn.query(sql, data, (err, result, fields)=>{
 			if (err) throw err;
 			console.log(result);
+			console.log("Customer data received successfully.")
 			conn.end((err)=>{
 				if (err) throw err;
 			});
 		});
 	});	
 	res.redirect("/thanks");
+	
+//  Holiday Packages population
+	app.get("/holiday-packages", (req, res)=>{
+		var conn = mysql.createConnection({
+			host: "localhost",
+			user: "Wintech",
+			password: "password",
+			database: "travelexperts"
+		})
+		
+		conn.connect((err) => {
+			if (err) throw err;
+			var sql = "SELECT * from `packages`";
+			conn.query(sql, (err, result, fields) => {
+				if (err) throw err;
+				res.writeHead(200, {"Content-Type":"text/html"})
+				fs.readFile(__dirname + "/holiday_packages.html", (err, data) => {
+					if (err) throw err;
+					res.write(data)
+				
+				// <table> tag --> <tr> create rows --> <td> create columns
+				res.write("<table border='1'>")
+				res.write("<tr>")
+				for (column of fields)
+				{
+					res.write("<th>" + column.name + "</th>"); 
+				}
+				res.write("</tr>")
+				for (pkg of result)
+				{
+					res.write("<tr>")
+					var values = Object.values(pkg);
+					for (i=0; i < values.length; i++)
+					{
+						res.write("<td>" + values[i] + "</td>") 
+					}
+					res.write("</tr>")
+				}
+				res.write("</body></html>");
+	
+				res.end();
+				
+				})
+			})
+		})
+	})
 });
